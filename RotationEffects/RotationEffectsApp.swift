78789ee7b00015f50ee88be7bd73f59a5ba40b63
Russell Gordon <rgordon@lcs.on.ca>
2024-11-17 10:23:42 -0500

    Initial Commit

---
+++ RotationEffects/RotationEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationEffectsApp.swift
+//  RotationEffects
+//
+//  Created by Russell Gordon on 2024-11-17.
+//
+
+import SwiftUI
+
+@main
+struct RotationEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
